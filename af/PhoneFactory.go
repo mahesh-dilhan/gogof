ec681ebe20106bd11112192ea0d878804870464e
mahesh <mail.mahesh.lk@gmail.com>
2022-01-03 10:34:52 +0800

    af-gof

--- af/PhoneFactory.go
+++ af/PhoneFactory.go
@@ -11,8 +11,8 @@ func getPhoneFactory(model string) (PhoneFactory, error) {
 	if model == "iphone" {
 		return &iphone{}, nil
 	}
-	if model == "nokia" {
-		return &nokia{}, nil
-	}
+	//if model == "nokia" {
+	//	return &nokia{}, nil
+	//}
 	return nil, fmt.Errorf("Wrong model passed")
 }
