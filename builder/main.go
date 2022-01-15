bc081df55f142ff5afb57d18a88d310d54b97065
mahesh <mail.mahesh.lk@gmail.com>
2022-01-15 15:08:45 +0800

    builder-gof

--- builder/main.go
+++ builder/main.go
@@ -5,10 +5,8 @@ import "fmt"
 func main() {
 	normalBuilder := getBuilder("normal")
 	iglooBuilder := getBuilder("igloo")
-
 	director := newDirector(normalBuilder)
 	normalHouse := director.buildHouse()
-
 	fmt.Printf("Normal House Door Type: %s\n", normalHouse.doorType)
 	fmt.Printf("Normal House Window Type: %s\n", normalHouse.windowType)
 	fmt.Printf("Normal House Num Floor: %d\n", normalHouse.floor)
