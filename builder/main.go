a335218362885165c032ce1ee494761164b9b96c
mahesh <mail.mahesh.lk@gmail.com>
2022-01-16 15:09:02 +0800

    builder-gof

--- builder/main.go
+++ builder/main.go
@@ -10,10 +10,8 @@ func main() {
 	fmt.Printf("Normal House Door Type: %s\n", normalHouse.doorType)
 	fmt.Printf("Normal House Window Type: %s\n", normalHouse.windowType)
 	fmt.Printf("Normal House Num Floor: %d\n", normalHouse.floor)
-
 	director.setBuilder(iglooBuilder)
 	iglooHouse := director.buildHouse()
-
 	fmt.Printf("\nIgloo House Door Type: %s\n", iglooHouse.doorType)
 	fmt.Printf("Igloo House Window Type: %s\n", iglooHouse.windowType)
 	fmt.Printf("Igloo House Num Floor: %d\n", iglooHouse.floor)
